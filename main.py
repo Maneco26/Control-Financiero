fbb9f8bf75806eef59e5e072d2f0afe4c2733b51
Maneco26 <vanegas.jefferson@hotmail.com>
2025-08-14 10:40:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 import streamlit as st
 from db.connection import init_db
 from modules.tarjetas import ui_tarjetas
+from modules.servicios import ui_servicios
+from modules.pagos import ui_pagos
 
 init_db()
 
 st.sidebar.title("📂 Módulos")
-modulo = st.sidebar.radio("Selecciona un módulo", ["Tarjetas"])
-
-if modulo == "Tarjetas":
-    ui_tarjetas()
-from modules.servicios import ui_servicios
-
-modulo = st.sidebar.radio("Selecciona un módulo", ["Tarjetas", "Servicios"])
-
-if modulo == "Tarjetas":
-    ui_tarjetas()
-elif modulo == "Servicios":
-    ui_servicios()
-
-from modules.pagos import ui_pagos
-
 modulo = st.sidebar.radio("Selecciona un módulo", ["Tarjetas", "Servicios", "Pagos"])
 
 if modulo == "Tarjetas":
